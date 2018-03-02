857e833760c7f3b8f1128c4ea3df6b238ba0db11
Anthony Wong <anthony.wong182@gmail.com>
2018-03-02 09:07:33 -0800

    edited app

--- src/App.js
+++ src/App.js
@@ -20,24 +20,8 @@ class App extends Component {
   // Register user to Firebase
   register = (e) => {
     e.preventDefault()
-<<<<<<< HEAD
     let email = e.target.userEmail.value
     let password = e.target.userPassword.value
-=======
-<<<<<<< HEAD
-    let user = {
-      username: e.target.userName.value,
-      email: e.target.userEmail.value,
-      userVoted: false
-    }
-=======
-    // let user = {
-    //   username: e.target.userName.value,
-    //   email: e.target.userEmail.value
-    // }
-    let email = e.target.userName.value
-    let password = e.target.userEmail.value
->>>>>>> eb58b27782f58a65ebab00cbcc23da6cfe1bd8f9
     fire.auth().createUserWithEmailAndPassword(email, password)
       .catch(function (err) {
         console.log('signed up')
@@ -59,7 +43,6 @@ class App extends Component {
         let errorMessage = error.message;
         console.log(errorCode, errorMessage)
     });
->>>>>>> ab0ebd5b584cc0a06eba179b08a2055318feef8c
     this.setState({
       isLoggedIn: true,
       user: user
@@ -130,7 +113,6 @@ class App extends Component {
           logout={this.logout} 
           isLoggedIn={this.state.isLoggedIn} />
 
-<<<<<<< HEAD
         <Switch>
           {/* Route for sign up */}
           <Route path="/" render={() => {
@@ -147,17 +129,6 @@ class App extends Component {
           }} />
         </Switch>
 
-        {/* {(!this.state.isLoggedIn) ? (
-          <Signin hacker={this.hacker}/>
-=======
-        {(!this.state.isLoggedIn) ? (
-          <Signin login={this.login} hacker={this.hacker}/>
->>>>>>> eb58b27782f58a65ebab00cbcc23da6cfe1bd8f9
-        ):(
-          <Main 
-            {...this.state}
-            votePoll={this.votePoll} />
-        )} */}
       </div>
     );
   }
