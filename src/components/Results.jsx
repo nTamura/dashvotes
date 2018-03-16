2aa55cd1b6ba8e98f5969c1a633f7a42d74ad479
Anthony Wong <anthony.wong182@gmail.com>
2018-03-16 11:30:12 -0700

    test remote branch

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -21,23 +21,15 @@ copmponentDidMount(){
     // const getData
 
     const data = {
-      labels: ["Team1", "Team2", "Team3"],
+      labels: ["Noodle Babies", "The Unicorn Hub", "Hackstreet Boiz"],
       datasets: [
         {
-          label: "Nic dataset",
+          label: "Votes",
           fillColor: "rgba(100, 100, 185, 1)",
           strokeColor: "rgba(220,220,220,0.8)",
           highlightFill: "rgba(220,220,220,0.75)",
           highlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-          label: "Ant dataset",
-          fillColor: "rgba(100, 100, 185, 1)",
-          strokeColor: "rgba(151,187,205,0.8)",
-          highlightFill: "rgba(151,187,205,0.75)",
-          highlightStroke: "rgba(151,187,205,1)",
-          data: [28, 48, 40, 19, 86, 27, 90]
+          data: [20, 13, 5]
         }
       ]
     };
@@ -47,10 +39,10 @@ copmponentDidMount(){
         <p>The results are in!</p>
         <Bar
           data={data}
-          width={100}
-          height={500}
+          width={150}
+          height={75}
           options={{
-            maintainAspectRatio: false
+            maintainAspectRatio: true
           }}
         />
       </div>
