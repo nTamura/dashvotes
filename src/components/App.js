970fd4e41692f084605bfdc5a2a80939a492333f
Nic Tamura <nictamura@gmail.com>
2019-05-11 19:28:53 -0700

    fresh start

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <nav>hello</nav>
+      <div className="App">
+        <header className="App-header">
+          <p>Dashvotes</p>
+        </header>
+      </div>
+      <footer>footer</footer>
+    </>
+  )
+}
+
+export default App
