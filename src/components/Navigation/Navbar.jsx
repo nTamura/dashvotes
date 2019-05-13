e95f4c0aa2edee2460271aa3094a214d35b8430e
Nic Tamura <nictamura@gmail.com>
2019-05-13 15:37:03 -0700

    nav add logo

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import withStyles from 'react-jss'
 import { Link } from 'react-router-dom'
+import logo from 'assets/dashSm.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar({ classes }) {
   return (
     <nav className={classes.root}>
-      <Link to="/">Home</Link>
-      <p>Menu</p>
+      <Link to="/">
+        <img src={logo} className={classes.logo} alt="Dash Logo" />
+      </Link>
+      <FontAwesomeIcon icon={faBars} className={classes.menu} />
     </nav>
   )
 }
 
 const styles = {
   root: {
+    height: 56,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  logo: {
+    height: 32,
+    // padding: 8,
+  },
+  menu: {
+    fontSize: '1.5rem',
+    margin: 8,
   },
 }
 
