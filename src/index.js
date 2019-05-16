aeb218fec5a83745c173e053f5602669f9c04f6c
Nic Tamura <nictamura@gmail.com>
2019-05-16 16:32:40 -0700

    firebaseAuthIsReady

--- src/index.js
+++ src/index.js
@@ -3,14 +3,34 @@ import ReactDOM from 'react-dom'
 import 'index.css'
 import App from 'components/App'
 import { BrowserRouter as Router } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from 'serviceWorker'
+import rootReducer from 'store/reducers/rootReducer'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { getFirestore, reduxFirestore } from 'redux-firestore'
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase'
+import firebase from 'config/firebaseConfig'
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase),
+    reactReduxFirebase(firebase, { attachAuthIsReady: true })
+  )
 )
+
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('root')
+  )
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
