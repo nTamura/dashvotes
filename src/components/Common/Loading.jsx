0d743496a56810e10b19402ff588463089d7c2a0
Nic Tamura <nictamura@gmail.com>
2019-05-16 23:10:39 -0700

    loading, sortby menu

---
+++ src/components/Common/Loading.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import withStyles from 'react-jss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+const Loading = ({ classes }) => (
+  <div style={styles.root}>
+    <FontAwesomeIcon spin className={classes.icon} icon={faSpinner} />
+    <p>No data available</p>
+  </div>
+)
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '40%',
+  },
+  icon: {
+    fontSize: '4rem',
+    marginBottom: 40,
+  },
+}
+
+export default withStyles(styles)(Loading)
--- src/components/Views/Trending/Container.jsx
+++ src/components/Views/Trending/Container.jsx
@@ -1,23 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
 import withStyles from 'react-jss'
 import PollsList from 'components/Common/Polls/PollsList'
+import Loading from 'components/Common/Loading'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faSortDown } from '@fortawesome/free-solid-svg-icons'
+function Container({ classes, polls, ...props }) {
+  const [showMenu, setShowMenu] = useState(false)
 
-function Container({ classes, ...props }) {
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+  const sortBy = e => {
+    const query = e.target.id
+    // sort polls by query
+  }
   return (
     <div className={classes.root}>
       <h2>Trending</h2>
       <div className={classes.flex}>
-        <p className={classes.subTitle}>Browsing top polls</p>
-        <div className={classes.sortContainer}>
-          <p className={classes.sortBy}>Sort by</p>
-          {/* <FontAwesomeIcon icon={faSortDown} className={classes.icon} /> */}
-        </div>
+        <p>Browsing top polls</p>
+        <button type="button" className={classes.sortBy} onClick={toggleMenu}>
+          Sort by <FontAwesomeIcon icon={faSortDown} className={classes.icon} />
+        </button>
+        {!showMenu && (
+          <div className={classes.menu}>
+            <ul className={classes.menuList}>
+              <li
+                id="popularity"
+                className={classes.menuItem}
+                onClick={e => {
+                  sortBy(e)
+                }}
+              >
+                Popularity
+              </li>
+              <li
+                id="newest"
+                className={classes.menuItem}
+                onClick={e => {
+                  sortBy(e)
+                }}
+              >
+                Newest
+              </li>
+              <li
+                id="oldest"
+                className={classes.menuItem}
+                onClick={e => {
+                  sortBy(e)
+                }}
+              >
+                Oldest
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-      <PollsList />
+      {polls ? <PollsList polls={polls} /> : <Loading />}
     </div>
   )
 }
@@ -26,18 +69,42 @@ const styles = {
   flex: {
     display: 'flex',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'relative',
   },
-  subTitle: {
-    marginTop: 0,
+  menu: {
+    position: 'absolute',
+    backgroundColor: '#444661',
+    right: 0,
+    top: 38,
+    borderRadius: 5,
   },
-  sortContainer: {
-    display: 'flex',
+  menuList: {
+    padding: 0,
   },
+  menuItem: {
+    padding: 8,
+    margin: 16,
+    listStyle: 'none',
+  },
+
   sortBy: {
+    color: '#FFF',
     margin: 0,
-    // marginTop: 0,
-    // float: 'right',
+    fontSize: '1rem',
+    fontWeight: 'inherit',
   },
-  icon: {},
+  icon: {
+    marginLeft: 4,
+  },
 }
-export default withStyles(styles)(Container)
+const mapStateToProps = state => {
+  // console.log(state)
+  return { polls: state.firestore.ordered.polls }
+}
+
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'polls' }])
+)(Container)
