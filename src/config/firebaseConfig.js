00329adeeb304b5fa7fd5f13f6b836d49552a7be
Nic Tamura <nictamura@gmail.com>
2019-05-17 17:32:59 -0700

    firebase config change

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,4 +1,4 @@
-import app from 'firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
@@ -12,60 +12,6 @@ const config = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
-class Firebase {
-  constructor() {
-    app.initializeApp(config)
-    this.auth = app.auth()
-    this.db = app.firestore
-  }
+firebase.initializeApp(config)
 
-  async signin({ email, password }) {
-    await this.auth.signInWithEmailAndPassword(email, password)
-  }
-
-  signinGoogle() {
-    //  this.auth.signInWithEmailAndPassword(email, password)
-    // this.auth
-    //   .signInWithEmailAndPassword(email, password)
-    //   .then(u => {
-    //     console.log('successfully logged in')
-    //   })
-    //   .catch(err => {
-    //     console.log('unable to log in')
-    //   })
-  }
-
-  async signout() {
-    await this.auth.signOut()
-  }
-
-  async register({ fname, lname, email, password }) {
-    if (!fname || !lname) {
-      throw new Error('custom/fields-missing')
-    } else if (fname.length < 3 || lname.length < 3) {
-      throw new Error('custom/fields-too-short')
-    } else {
-      await this.auth.createUserWithEmailAndPassword(email, password)
-      return this.auth.currentUser.updateProfile({
-        displayName: `${fname} ${lname.charAt(0)}`,
-        fname,
-        lname,
-        email,
-        password,
-        createdAt: Date.now(),
-      })
-    }
-  }
-
-  isInit() {
-    return new Promise(resolve => {
-      this.auth.onAuthStateChanged(resolve)
-    })
-  }
-
-  resetPassword = email => this.auth.sendPasswordResetEmail(email)
-
-  updatePassword = password => this.auth.currentUser.updatePassword(password)
-}
-
-export default Firebase
+export default firebase
