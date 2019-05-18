61520a2fff5aa8bfad757b23d1f7c4f660b40a3b
Nic Tamura <nictamura@gmail.com>
2019-05-17 17:33:49 -0700

    results page needs connect

--- src/components/Views/Results/Container.jsx
+++ src/components/Views/Results/Container.jsx
@@ -18,27 +18,5 @@ function Container({ classes }) {
 }
 const styles = {
   root: {},
-  menuList: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  item: {
-    border: '2px solid #FFF',
-    borderRadius: 5,
-    padding: 8,
-    margin: '8px 0',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  itemTitle: {
-    flex: 1,
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    marginRight: 8,
-    padding: '8px 0',
-    fontWeight: 'bold',
-  },
 }
 export default withStyles(styles)(Container)
