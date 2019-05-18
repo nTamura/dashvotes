7f9f1c5a036789506f1a1344de7c7b3005202a8f
Nic Tamura <nictamura@gmail.com>
2019-05-17 17:20:11 -0700

    create page

--- src/components/Views/Create/Container.jsx
+++ src/components/Views/Create/Container.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import withStyles from 'react-jss'
 import FormContainer from 'components/Views/Create/FormContainer'
 import ThanksCreate from 'components/Views/Create/ThanksCreate'
-import { createPoll } from 'store/actions/pollsActions'
+import { createPoll, clearPid } from 'store/actions/pollsActions'
 import { connect } from 'react-redux'
 
-function Container({ classes, auth, pid, submitting, createPoll }) {
-  const formSubmit = async form => {
-    await createPoll(form)
-    // } else {
-    // dispatch form error
-    // console.log('error')
-    // }
+function Container({ classes, auth, pid, clearPid, submitting, createPoll }) {
+  const [submitted, setSubmitted] = useState(false)
+  useEffect(() => {
+    if (pid) {
+      setSubmitted(true)
+    }
+    return () => clearPid()
+  }, [pid])
+
+  const formSubmit = form => {
+    createPoll(form)
   }
 
   return (
     <div className={classes.root}>
       {/* pid acts as successfully submitted, after retrieving id from firestore */}
-      {!pid ? (
+      {!submitted ? (
         <FormContainer
           auth={auth}
           submitting={submitting}
@@ -35,6 +39,7 @@ const styles = {
 
 const mapDispatchToProps = dispatch => ({
   createPoll: poll => dispatch(createPoll(poll)),
+  clearPid: () => dispatch(clearPid()),
 })
 const mapStateToProps = state => ({
   auth: state.firebase.auth,
