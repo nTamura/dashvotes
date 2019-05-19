aa18de303dc1f6cb61798eef9f21fa76420bc28d
Nic Tamura <nictamura@gmail.com>
2019-05-18 18:33:06 -0700

    add id to create poll

--- src/store/actions/pollsActions.js
+++ src/store/actions/pollsActions.js
@@ -6,11 +6,16 @@ export const createPoll = poll => (dispatch, getState, { getFirestore }) => {
     .add({
       ...poll,
       createdAt: Date.now(),
+      votes: [],
       expireAt: '',
       isPublic: true,
     })
     .then(docRef => {
       const { id } = docRef
+      firestore
+        .collection('polls')
+        .doc(id)
+        .update({ pid: id })
       firestore
         .collection('users')
         .doc(poll.createdBy.uid)
@@ -24,6 +29,40 @@ export const createPoll = poll => (dispatch, getState, { getFirestore }) => {
       console.log('CreatePoll error:', err)
     })
 }
+
+export const votePoll = (id, user) => (
+  dispatch,
+  getState,
+  { getFirestore }
+) => {
+  const firestore = getFirestore()
+  dispatch({ type: 'TRY_VOTE_POLL' })
+  firestore
+    .collection('polls')
+    .doc(id)
+    .get()
+    .then(docRef => {
+      console.log(docRef)
+
+      // .update({
+      //   votes: firestore.FieldValue.arrayUnion(user)
+      // })
+      // .then(docRef => {
+      //   const { id } = docRef
+      //   firestore
+      //     .collection('users')
+      //     .doc(poll.createdBy.uid)
+      //     .update({
+      //       pollsCreated: firestore.FieldValue.arrayUnion(id),
+      //     })
+      //   dispatch({ type: 'CREATE_POLL', payload: id })
+      // })
+      // .catch(err => {
+      //   dispatch({ type: 'CREATE_POLL_ERR', err })
+      //   console.log('CreatePoll error:', err)
+    })
+}
+
 export const fetchPoll = id => (dispatch, getState, { getFirestore }) => {
   const firestore = getFirestore()
   dispatch({ type: 'TRY_FETCH_POLL' })
