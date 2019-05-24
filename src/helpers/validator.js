f764e71feec238d602f87dbaaafe329eb65b7042
Nic Tamura <nictamura@gmail.com>
2019-05-24 14:02:07 -0700

    modify validator helper

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,15 +1,18 @@
 // client side form validation
 
 export const validateFields = values => {
-  // const { fname, lname, email, password } = values
   let errors = {}
 
   // name validate
   if (!values.fname) {
     errors.fname = 'Field is required'
+  } else if (values.fname.length < 2) {
+    errors.fname = 'Field is too short'
   }
   if (!values.lname) {
     errors.lname = 'Field is required'
+  } else if (values.lname.length < 2) {
+    errors.lname = 'Field is too short'
   }
 
   // email validate
@@ -24,43 +27,9 @@ export const validateFields = values => {
   // password validate
   if (!values.password) {
     errors.password = 'Field is required'
-  } else if (values.password.length <= 6) {
-    errors.password = 'Field is too short'
+  } else if (values.password.length < 6) {
+    errors.password = 'Password is too short'
   }
 
   return errors
 }
-
-// export const validateName = str => {
-//   let err = {}
-//   if (typeof str === 'string') {
-//     if (str === '') {
-//       return 'Field is required'
-//     } else if (str.length <= 2) {
-//       return 'Field is too short'
-//     }
-//   } else return 'Invalid input'
-// }
-
-// export const validateEmail = email => {
-//   let err = {}
-//   const match = /\S+@\S+\.\S+/
-//   if (typeof email === 'string') {
-//     if (email === '') {
-//       err.email = 'Field is required'
-//     } else if (!match.test(email)) {
-//       err.email = 'Valid email is required'
-//     } else return email
-//   } else return 'Invalid input'
-// }
-
-// export const validatePW = pw => {
-//   let err = {}
-//   if (typeof pw === 'string') {
-//     if (pw === '') {
-//       return 'Field is required'
-//     } else if (pw.length <= 6) {
-//       return 'Field is too short'
-//     }
-//   } else return 'Invalid input'
-// }
