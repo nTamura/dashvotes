9807a86005e57e74e944e143a01e7b4f3eca7822
Nic Tamura <nictamura@gmail.com>
2019-06-01 20:03:28 -0700

    vote polls, arrayUnion

--- src/store/actions/pollsActions.js
+++ src/store/actions/pollsActions.js
@@ -34,33 +34,35 @@ export const votePoll = poll => (dispatch, getState, { getFirestore }) => {
   const firestore = getFirestore()
   dispatch({ type: 'TRY_VOTE_POLL' })
   const { pid, options, voter } = poll
-  const x = {
-    [options]: [voter],
-  }
-  console.log(poll)
+  const vote = { [options]: voter }
   firestore
     .collection('polls')
     .doc(pid)
-    // .update({
-    //   votes: firestore.FieldValue.arrayUnion(x),
-    // })
-    .then(() => {
-      console.log(poll)
+    // .get()
+    // .then(doc => {
+    //   if (doc.exists) {
 
-      //   firestore.collection('users').doc(voter.id)
-      //   const { id } = docRef
-      //   firestore
-      //     .collection('users')
-      //     .doc(poll.createdBy.uid)
-      //     .update({
-      //       pollsCreated: firestore.FieldValue.arrayUnion(id),
-      //     })
-      //   dispatch({ type: 'CREATE_POLL', payload: id })
-      // })
-      // .catch(err => {
-      //   dispatch({ type: 'CREATE_POLL_ERR', err })
-      //   console.log('CreatePoll error:', err)
+    .update({
+      votes: firestore.FieldValue.arrayUnion({ [options]: voter }),
     })
+  // .update({
+  //   votes: firestore.FieldValue.arrayUnion(x),
+  // })
+  //   firestore.collection('users').doc(voter.id)
+  //   const { id } = docRef
+  //   firestore
+  //     .collection('users')
+  //     .doc(poll.createdBy.uid)
+  //     .update({
+  //       pollsCreated: firestore.FieldValue.arrayUnion(id),
+  //     })
+  //   dispatch({ type: 'CREATE_POLL', payload: id })
+  // })
+  // .catch(err => {
+  //   dispatch({ type: 'CREATE_POLL_ERR', err })
+  //   console.log('CreatePoll error:', err)
+  // }
+  // })
 }
 
 export const fetchPoll = id => (dispatch, getState, { getFirestore }) => {
