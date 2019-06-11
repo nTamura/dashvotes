a0edbd39a4c7fc3583c950fe1a3d6cad1c5d9788
Nic Tamura <nictamura@gmail.com>
2019-06-11 11:50:15 -0700

    add logic toDate

--- src/helpers/toDate.js
+++ src/helpers/toDate.js
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 
-export const toDate = timestamp => {
+export const toDate = arg => {
+  const timestamp = Number(arg)
   const dt = DateTime.fromMillis(timestamp)
   const format = Object.assign({ weekday: 'short' }, DateTime.DATE_MED)
   return dt.toLocaleString(format)
