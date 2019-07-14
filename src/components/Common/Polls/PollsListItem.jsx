e6971ca74018e812ff100eb43a8a64ee4cf18f98
Nic Tamura <nictamura@gmail.com>
2019-07-14 15:06:56 -0700

    vote counts

--- src/components/Common/Polls/PollsListItem.jsx
+++ src/components/Common/Polls/PollsListItem.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 import withStyles from 'react-jss'
 
 function PollItem({ classes, poll, ...props }) {
+  // console.log(poll)
   // const id = props.match.params.id
   return (
     <div className={classes.root}>
       <Link className={classes.itemTitle} to={`poll/${poll.id}`}>
         {poll.title}
       </Link>
-      <p>1150 votes</p>
+      <p>{`${poll.votes.length} votes`}</p>
     </div>
   )
 }
