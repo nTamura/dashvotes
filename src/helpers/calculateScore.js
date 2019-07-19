68d5629fd1f6522d571fb94a922922a6bd8df95a
Nic Tamura <nictamura@gmail.com>
2019-07-18 17:43:26 -0700

    helper methods

--- src/helpers/calculateScore.js
+++ src/helpers/calculateScore.js
@@ -2,7 +2,7 @@ const calculateScore = arr => {
   if (!Array.isArray(arr)) {
     throw new Error('calculateScore args must be in an array')
   } else {
-    return arr.map(item => (1 + item) * 10).reduce((a, b) => a * b)
+    return arr.map(item => item * 3).reduce((a, b) => a + b)
   }
 }
 
--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -33,3 +33,42 @@ export const validate = values => {
 
   return errors
 }
+export const validateCreate = values => {
+  let errors = {}
+
+  Object.values(values).forEach(v => {
+    if (!v) {
+      console.log('missing')
+    } else return true
+  })
+
+  // name validate
+  if (!values.fname) {
+    errors.fname = 'Field is required'
+  } else if (values.fname.length < 2) {
+    errors.fname = 'Name is too short'
+  }
+  if (!values.lname) {
+    errors.lname = 'Field is required'
+  } else if (values.lname.length < 2) {
+    errors.lname = 'Name is too short'
+  }
+
+  // email validate
+  const match = /\S+@\S+\.\S+/
+
+  if (!values.email) {
+    errors.email = 'Field is required'
+  } else if (!match.test(values.email)) {
+    errors.email = 'Valid email is required'
+  }
+
+  // password validate
+  if (!values.password) {
+    errors.password = 'Field is required'
+  } else if (values.password.length < 6) {
+    errors.password = 'Password is too short'
+  }
+
+  return errors
+}
